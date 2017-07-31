450501f17d42fc09ad24b178381357b1ddadb45b
engstk <eng.stk@sapo.pt>
2017-07-31 18:28:24 +0100

    drivers: misc: implement usb fast charge mode
    echo 0 /sys/kernel/fast_charge/force_fast_charge (disable)
    echo 1 /sys/kernel/fast_charge/force_fast_charge (enable)
    
    Enables force charging up to 900mA in usb mode
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -37,6 +37,10 @@ module_param_string(mode, bootmode, BOOTMODE_LENGTH, 0000);
 	logbuffer_log(chg->log, "%s: %s: " fmt,			\
 		      chg->name, __func__, ##__VA_ARGS__)	\
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 #define smblib_dbg(chg, reason, fmt, ...)			\
 	do {							\
 		if (*chg->debug_mask & (reason))		\
@@ -878,6 +882,13 @@ static int set_sdp_current(struct smb_charger *chg, int icl_ua)
 	int rc;
 	u8 icl_options;
 	const struct apsd_result *apsd_result = smblib_get_apsd_result(chg);
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge > 0 && icl_ua == USBIN_500MA)
+	{
+		icl_ua = USBIN_900MA;
+	}
+#endif
 
 	/* power source is SDP */
 	switch (icl_ua) {
